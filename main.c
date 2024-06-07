4f569e3bcc2946c64c75496f22e1d894b019f519
LLulztron <163662385+LLulztron@users.noreply.github.com>
2024-06-07 19:03:23 +0000

    arreglo de char en el struct

--- main.c
+++ main.c
@@ -4,13 +4,15 @@
 #include <string.h>
 #include <time.h>
 
-typedef struct{
+typedef struct
+{
 int id;
-char* titulo;
-time_t hora_registro;
-int prioridad;
+char* titulo[100];
+char* descripcionTarea[200];
+time_t fecha_creacion,fecha_limite;
+int prioridad,dificultad;
 List* list;
-}Tarea;
+} Tarea;
 
 void limpiarPantalla() { system("clear"); }
 void presioneTeclaParaContinuar()
@@ -27,21 +29,29 @@ void mostrarMenuPrincipal()
   puts("========================================");
 
   puts("1) Agregar tarea pendiente");
-  
+  puts("2) Asignar prioridad");
   puts("3) Mostrar tareas pendientes");
-  puts("4) Eliminar tarea completada ");
+  puts("4) Modificar Tarea ");
   puts("5) Buscar tarea");
-  puts("6) Modificar Tarea");
+  puts("6) Eliminar tarea completada");
   puts("7) Salir");
 }
 /*SUBMENU MODO BETA
 
-  */
+  puts("========================================");
+  puts("     Tarea");
+  puts("========================================");
+  puts("1) Mostrar tarea");
+  puts("2) Modificar tarea");
+  puts("3) Buscar tarea");
+  puts("4) Salir");
+  
+*/
 
 int main(void)
 {
-  
   char opcion;
+  //List *tareas = createList();
   do
   {
     mostrarMenuPrincipal();
@@ -60,13 +70,13 @@ int main(void)
       //MostrarTareasPendientes();
       break;
     case '4':
-      //EliminarTareaCompletada();
+      //ModificarTarea();
       break;
     case '5':
       //BuscarTareas();
       break;
     case '6':
-      //ModificarTarea();
+      //EliminarTareaCompletada();
       break;
       case '7':
       printf("Saliendo...\n");
